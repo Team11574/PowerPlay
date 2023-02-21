fd580967e1b9e5cd8d6df7106108471a91ae10d0
wil <seank1215@icloud.com>
2023-02-20 17:53:41 -0800

    More Slide code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/slides/Slide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/slides/Slide.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.robot.exceptions.UndefinedSetPositionException;
 
 class Slide {
-    double MAX_SPEED;
+    double MAX_POWER;
     double MIN_ENCODER_POSITION;
     double MAX_ENCODER_POSITION;
     double run_to_position_power = 1;
@@ -19,44 +19,49 @@ class Slide {
         this(new DcMotor[]{slideMotor});
     }
 
-    public Slide(DcMotor slideMotor, double maxSpeed) {
-        this(new DcMotor[]{slideMotor}, maxSpeed);
+    public Slide(DcMotor slideMotor, double maxPower) {
+        this(new DcMotor[]{slideMotor}, maxPower);
     }
 
     public Slide(DcMotor[] slideMotors) {
         this(slideMotors, 1);
     }
 
-    public Slide(DcMotor[] slideMotors, double maxSpeed) {
+    public Slide(DcMotor[] slideMotors, double maxPower) {
         motors = slideMotors;
         MIN_ENCODER_POSITION = 0;
         MAX_ENCODER_POSITION = Double.POSITIVE_INFINITY;
-        MAX_SPEED = maxSpeed;
+        MAX_POWER = maxPower;
     }
     public Slide(DcMotor slideMotor, double minEncoderPosition, double maxEncoderPosition) {
         this(new DcMotor[]{slideMotor}, minEncoderPosition, maxEncoderPosition, 1);
     }
 
-    public Slide(DcMotor slideMotor, double minEncoderPosition, double maxEncoderPosition, double maxSpeed) {
-        this(new DcMotor[]{slideMotor}, minEncoderPosition, maxEncoderPosition, maxSpeed);
+    public Slide(DcMotor slideMotor, double minEncoderPosition, double maxEncoderPosition, double maxPower) {
+        this(new DcMotor[]{slideMotor}, minEncoderPosition, maxEncoderPosition, maxPower);
     }
 
     public Slide(DcMotor[] slideMotors, double minEncoderPosition, double maxEncoderPosition) {
         this(slideMotors, minEncoderPosition, maxEncoderPosition, 1);
     }
 
-    public Slide(DcMotor[] slideMotors, double minEncoderPosition, double maxEncoderPosition, double maxSpeed) {
+    public Slide(DcMotor[] slideMotors, double minEncoderPosition, double maxEncoderPosition, double maxPower) {
         motors = slideMotors;
         MIN_ENCODER_POSITION = minEncoderPosition;
         MAX_ENCODER_POSITION = maxEncoderPosition;
-        MAX_SPEED = maxSpeed;
+        MAX_POWER = maxPower;
     }
 
     public void setRunToPositionPower(double new_power) {
+        if (new_power > MAX_POWER)
+            new_power = MAX_POWER;
         run_to_position_power = new_power;
+
     }
 
     public void setPower(double power) {
+        if (power > MAX_POWER)
+            power = MAX_POWER;
         for (DcMotor motor : motors) {
             motor.setPower(power);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -66,6 +71,9 @@ class Slide {
     public void setTargetPosition(int position) {
         for (DcMotor motor : motors) {
             // Run to position needs some power value to run at, default is 1
+            if (run_to_position_power > MAX_POWER) {
+                run_to_position_power = MAX_POWER;
+            }
             motor.setPower(run_to_position_power);
             motor.setTargetPosition(position);
             motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/slides/VerticalSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/components/slides/VerticalSlide.java
@@ -8,16 +8,37 @@ public class VerticalSlide extends Slide {
         super(slideMotor);
     }
 
-    public VerticalSlide(DcMotor slideMotor, double maxHeight, double minEncoderPosition, double maxEncoderPosition) {
-        super(slideMotor, maxHeight, minEncoderPosition, maxEncoderPosition);
+    public VerticalSlide(DcMotor slideMotor, double maxPower) {
+        super(slideMotor, maxPower);
     }
 
     public VerticalSlide(DcMotor[] slideMotors) {
         super(slideMotors);
     }
 
-    public VerticalSlide(DcMotor[] slideMotors, double maxHeight, double minEncoderPosition,
-            double maxEncoderPosition) {
-        super(slideMotors, maxHeight, minEncoderPosition, maxEncoderPosition);
+    public VerticalSlide(DcMotor[] slideMotors, double maxPower) {
+        super(slideMotors, maxPower);
+    }
+
+    public VerticalSlide(DcMotor slideMotor, double minEncoderPosition,
+                         double maxEncoderPosition) {
+        super(slideMotor, minEncoderPosition, maxEncoderPosition);
+    }
+
+
+    public VerticalSlide(DcMotor[] slideMotors, double minEncoderPosition,
+                         double maxEncoderPosition) {
+        super(slideMotors, minEncoderPosition, maxEncoderPosition);
+    }
+
+    public VerticalSlide(DcMotor slideMotor, double minEncoderPosition,
+                         double maxEncoderPosition, double maxPower) {
+        super(slideMotor, minEncoderPosition, maxEncoderPosition, maxPower);
+    }
+
+
+    public VerticalSlide(DcMotor[] slideMotors, double minEncoderPosition,
+                         double maxEncoderPosition, double maxPower) {
+        super(slideMotors, minEncoderPosition, maxEncoderPosition, maxPower);
     }
 }
